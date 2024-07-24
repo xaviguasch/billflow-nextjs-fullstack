6b331ce19111f51f2be390958c71fd5ec3838a15
xavi <xavi@MBP-de-xavi.home>
2024-07-24 17:02:19 +0200

    added InvoiceGroup and InvoicePreview components

---
+++ app/_components/InvoiceGroup.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const InvoiceGroup = () => {
+  return (
+    <div>
+      <div className="">
+        <h2>Invoices</h2>
+        <p>7 invoices</p>
+
+        <div>Filter / New</div>
+      </div>
+
+      <div>Invoices list</div>
+    </div>
+  );
+};
+
+export default InvoiceGroup;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import InvoiceGroup from "./_components/InvoiceGroup";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="bg-">BillFlow test</h1>
+    <main className="">
+      <InvoiceGroup />
     </main>
   );
 }
