4be1fe9e4a683f94cf4a785fed1cee72b2f0dfa3
xavi <xavi@MBP-de-xavi.home>
2024-07-26 17:37:41 +0200

    styled New Button, pending to wire

--- app/_components/NewButton.tsx
+++ app/_components/NewButton.tsx
@@ -6,12 +6,11 @@ import IconPlusSVG from "./IconPlusSVG";
 const NewButton = () => {
   return (
     <button>
-      <div>
+      <div className="flex w-[90px] flex-row items-center justify-start gap-2 rounded-3xl bg-medium-slate-blue p-[6px]">
         <div className="bg-white flex h-8 w-8 items-center justify-center rounded-full">
           <IconPlusSVG />
         </div>
-        <div className=""></div>
-        <span>New</span>
+        <span className="text-white text-xs font-bold">New</span>
       </div>
     </button>
   );
