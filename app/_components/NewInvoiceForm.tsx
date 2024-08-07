112a41814b8d5fbac340f24eb59f6a07d9f3afa1
xavi <xavi@MBP-de-xavi.home>
2024-08-07 12:35:48 +0200

    added /new page for new creating new invoices, also added NewInvoiceForm component

---
+++ app/_components/NewInvoiceForm.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const NewInvoiceForm = () => {
+  return (
+    <form action="">
+      <h2>New Invoice</h2>
+    </form>
+  );
+};
+
+export default NewInvoiceForm;
---
+++ app/new/page.tsx
@@ -0,0 +1,10 @@
+import NewInvoiceForm from "@/app/_components/NewInvoiceForm";
+
+const NewInvoice = async () => {
+  return (
+    <div>
+      <NewInvoiceForm />
+    </div>
+  );
+};
+export default NewInvoice;
