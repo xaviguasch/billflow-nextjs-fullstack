0ab6112e8acd39a774a245b27afc2e92e64e7fcd
xavi <xavi@MBP-de-xavi.home>
2024-08-08 13:00:37 +0200

    styled first part of NewInvoiceForm component

--- app/_components/NewInvoiceForm.tsx
+++ app/_components/NewInvoiceForm.tsx
@@ -2,9 +2,28 @@
 
 const NewInvoiceForm = () => {
   return (
-    <div className="py-6">
-      <h2>New Invoice</h2>
-      <form action=""></form>
+    <div className="flex flex-col items-stretch justify-start space-y-6 py-6">
+      <h2 className="text-2xl font-bold tracking-wider">New Invoice</h2>
+      <form
+        action=""
+        className="flex flex-col items-stretch justify-start space-y-6"
+      >
+        <span className="text-xs font-bold text-medium-slate-blue">
+          Bill From
+        </span>
+        <div className="flex flex-col items-stretch justify-start space-y-2.5">
+          <label
+            htmlFor="street-address"
+            className="text-xs text-wild-blue-yonder"
+          >
+            Street Address
+          </label>
+          <input type="text" id="street-address" />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </form>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,17 @@
 import Image from "next/image";
 import InvoiceGroup from "./_components/InvoiceGroup";
 
-async function fetchInvoices() {
-  try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/invoices`, {
-      method: "GET",
-      next: { revalidate: 10 },
-    });
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    return res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+import connectDB from "@/config/database";
+import Invoice from "@/models/Invoice";
 
 export const revalidate = 3600;
 
 const HomePage = async () => {
-  const invoices = await fetchInvoices();
+  connectDB();
+
+  const invoices = await Invoice.find({}).lean();
+
+  console.log(invoices);
 
   return (
     <main className="px-6 py-8">
