2aac69c2c51df090f0e363a2539172e43432d148
Xavi Guasch <xaviguasch@gmail.com>
2024-08-26 18:01:47 +0200

    implemented filter status functionality in InvoiceGroup component

--- app/_components/InvoiceGroup.tsx
+++ app/_components/InvoiceGroup.tsx
@@ -7,15 +7,25 @@ import FilterSelect from "./FilterSelect";
 
 type InvoiceGroupProps = {
   invoices: InvoiceDataItem[];
+  filter: "all" | "draft" | "pending" | "paid";
 };
 
-const InvoiceGroup = ({ invoices }: InvoiceGroupProps) => {
+const InvoiceGroup = ({ invoices, filter }: InvoiceGroupProps) => {
+  console.log(filter);
+
+  let selectedInvoices;
+  if (!filter || filter === "all") {
+    selectedInvoices = invoices;
+  } else {
+    selectedInvoices = invoices.filter((invoice) => invoice.status === filter);
+  }
+
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-row justify-between">
         <div>
           <h2 className="text-xl">Invoices</h2>
-          <p className="text-regent-gray text-xs">{invoices.length} invoices</p>
+          <p className="text-xs text-regent-gray">{invoices.length} invoices</p>
         </div>
 
         <div className="flex flex-row items-center justify-start gap-[18px]">
@@ -25,7 +35,7 @@ const InvoiceGroup = ({ invoices }: InvoiceGroupProps) => {
       </div>
 
       <div className="flex flex-col justify-start gap-y-4">
-        {invoices.map((invoice) => (
+        {selectedInvoices.map((invoice) => (
           <InvoicePreview item={invoice} key={invoice.original_id} />
         ))}
       </div>
