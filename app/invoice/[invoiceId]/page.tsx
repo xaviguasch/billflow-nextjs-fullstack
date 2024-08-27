8cc3521a1a8a6bd41f5f9d6adbdf7b36de186c8f
Xavi Guasch <xaviguasch@gmail.com>
2024-08-27 19:41:03 +0200

    added first stages of data retrieval from individual Invoice page

--- app/invoice/[invoiceId]/page.tsx
+++ app/invoice/[invoiceId]/page.tsx
@@ -1,9 +1,37 @@
-const InvoicePage = ({ params }) => {
+import connectDB from "@/config/database";
+
+import Invoice from "@/models/Invoice";
+import { connect } from "http2";
+
+async function findInvoice(id) {
+  try {
+    connectDB();
+    const invoice = await Invoice.findOne({ _id: id });
+
+    return invoice;
+  } catch (error) {
+    console.error("Error finding document:", error);
+  }
+}
+
+const InvoicePage = async ({ params }) => {
+  connectDB();
+
   console.log(params);
+
+  const invoice = await findInvoice(params.invoiceId);
+
+  if (!invoice) {
+    return <h2>NO INVOICE!!!! pending</h2>;
+  }
 
   return (
     <div>
       <p>This is the invoice page for id: {params.invoiceId}</p>
+      <p>{invoice.clientEmail}</p>
+      <p>{invoice.clientName}</p>
+      <p>{invoice.status}</p>
+      <p>{invoice.description}</p>
     </div>
   );
 };
