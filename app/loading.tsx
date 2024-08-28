b4222f64d88ce0c5116dae32d52602f7377e3c63
Xavi Guasch <xaviguasch@gmail.com>
2024-08-28 12:11:11 +0200

    added loading page component with a spinner from react-spinners package

---
+++ app/loading.tsx
@@ -0,0 +1,23 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+// NOTE: Here 'loading' will be undefined as a loading file doesn't receive any
+// props:
+// https://nextjs.org/docs/app/api-reference/file-conventions/loading
+
+const LoadingPage = () => {
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+export default LoadingPage;
