4abd09a6b0b633d3243db00ba1671c9731189def
Xavi Guasch <xaviguasch@gmail.com>
2024-09-03 17:14:07 +0200

    added first bunch of styling to InvoicePreview component

--- app/_components/InvoicePreview.tsx
+++ app/_components/InvoicePreview.tsx
@@ -19,9 +19,9 @@ const InvoicePreview = ({ item }: InvoiceProps) => {
     item.status[0].toUpperCase() + item.status.slice(1).toLowerCase();
 
   return (
-    <div className="text-wild-blue-yonder bg-white grid auto-rows-auto grid-cols-2 gap-y-6 rounded-lg p-6 text-xs font-medium">
+    <div className="grid auto-rows-auto grid-cols-2 gap-y-6 rounded-lg bg-white p-6 text-xs font-medium text-wild-blue-yonder">
       <p className="font-bold">
-        #<span className="text-cinder font-bold">{item.original_id}</span>
+        #<span className="text-cinder">{item.original_id}</span>
       </p>
       <div className="justify-self-end">
         <p className="">{item.clientName}</p>
@@ -29,13 +29,13 @@ const InvoicePreview = ({ item }: InvoiceProps) => {
 
       <div className="flex flex-col justify-between space-y-2">
         <p className="">{formattedDate}</p>
-        <p className="text-cinder text-base font-bold">
+        <p className="text-base font-bold text-cinder">
           £{new Intl.NumberFormat("en-US").format(item.total)}
         </p>
       </div>
 
       <div
-        className={`flex h-10 w-[104px] items-center justify-center space-x-2 self-center justify-self-end rounded-md font-bold ${item.status === "paid" && "text-aqua-green bg-aqua-green/5"} ${item.status === "pending" && "text-dark-orange bg-dark-orange/5"} ${item.status === "draft" && "text-bright-grey bg-bright-grey/5"}`}
+        className={`flex h-10 w-[104px] items-center justify-center space-x-2 self-center justify-self-end rounded-md font-bold ${item.status === "paid" && "bg-aqua-green/5 text-aqua-green"} ${item.status === "pending" && "bg-dark-orange/5 text-dark-orange"} ${item.status === "draft" && "bg-bright-grey/5 text-bright-grey"}`}
       >
         <span className="text-[32px]">·</span>
 
--- app/invoice/[invoiceId]/page.tsx
+++ app/invoice/[invoiceId]/page.tsx
@@ -21,17 +21,112 @@ const InvoicePage = async ({ params }) => {
 
   const invoice = await findInvoice(params.invoiceId);
 
+  console.log(invoice);
+
+  const invoiceCreationDate = new Date(invoice.createdAt.toString());
+  const paymentDueDate = new Date(invoice.paymentDue.toString());
+
+  // Define options for the Intl.DateTimeFormat
+  const options = { day: "numeric", month: "short", year: "numeric" };
+
+  // Format the date
+  const formatedInvoiceCreationDate = new Intl.DateTimeFormat(
+    "en-GB",
+    options,
+  ).format(invoiceCreationDate);
+
+  const formatedPaymentDueDate = new Intl.DateTimeFormat(
+    "en-GB",
+    options,
+  ).format(paymentDueDate);
+
+  const statusString =
+    invoice.status[0].toUpperCase() + invoice.status.slice(1).toLowerCase();
+
   if (!invoice) {
+    // PENDING FINISHING STYLING
     return <h2>NO INVOICE!!!! pending</h2>;
   }
 
   return (
     <div>
-      <p>This is the invoice page for id: {params.invoiceId}</p>
-      <p>{invoice.clientEmail}</p>
-      <p>{invoice.clientName}</p>
-      <p>{invoice.status}</p>
-      <p>{invoice.description}</p>
+      <div className="px-6 pt-8">
+        <p>This is the invoice page for id: {params.invoiceId}</p>
+        <p>{invoice.clientEmail}</p>
+        <p>{invoice.clientName}</p>
+        <p>{invoice.status}</p>
+        <p>{invoice.description}</p>
+
+        <div>
+          <button>Go Back</button>
+        </div>
+
+        <div className="flex flex-col items-stretch justify-start gap-4">
+          <div className="flex flex-row items-center justify-between rounded-lg bg-white px-6 py-6 text-xs">
+            <span className="text-wild-blue-yonder">Status</span>
+            <div
+              className={`flex h-10 w-[104px] items-center justify-center space-x-2 self-center justify-self-end rounded-md font-bold ${invoice.status === "paid" && "bg-aqua-green/5 text-aqua-green"} ${invoice.status === "pending" && "bg-dark-orange/5 text-dark-orange"} ${invoice.status === "draft" && "bg-bright-grey/5 text-bright-grey"}`}
+            >
+              <span className="text-[32px]">·</span>
+              <p className="">{statusString}</p>
+            </div>
+          </div>
+          <div className="flex flex-col justify-start gap-8 bg-white px-6 py-6 text-xs text-wild-blue-yonder">
+            <div>
+              <p>
+                #
+                <span className="font-bold text-cinder">
+                  {invoice.original_id}
+                </span>
+              </p>
+              <p className="">{invoice.description}</p>
+            </div>
+
+            <div className="text-[11px]">
+              <p>{invoice.senderAddress.street}</p>
+              <p>{invoice.senderAddress.city}</p>
+              <p>{invoice.senderAddress.postCode}</p>
+              <p>{invoice.senderAddress.country}</p>
+            </div>
+
+            <div className="grid grid-cols-2 gap-y-3 text-xs">
+              <div>
+                <p>Invoice Date</p>
+                <p className="mt-2 text-[15px] font-bold text-cinder">
+                  {formatedInvoiceCreationDate}
+                </p>
+              </div>
+              <div>
+                <p>Bill To</p>
+                <p className="mt-2 text-[15px] font-bold text-cinder">
+                  {invoice.clientName}
+                </p>
+              </div>
+              <div className="self-end">
+                <p>Payment Due</p>
+                <p className="mt-2 text-[15px] font-bold text-cinder">
+                  {formatedPaymentDueDate}
+                </p>
+              </div>
+              <div className="self-end">
+                <p>{invoice.clientAddress.street}</p>
+                <p>{invoice.clientAddress.city}</p>
+                <p>{invoice.clientAddress.postCode}</p>
+                <p>{invoice.clientAddress.county}</p>
+              </div>
+            </div>
+
+            <div>
+              <p>Sent to</p>
+              <p className="mt-2 text-[15px] font-bold text-cinder">
+                {invoice.clientEmail}
+              </p>
+            </div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
