13200a9b7ab2cde6473117928b57321b28832242
Xavi Guasch <xaviguasch@gmail.com>
2024-09-27 14:35:10 +0200

    refactored layout structure on InvoicePreview for mobile view

--- app/_components/InvoicePreview.tsx
+++ app/_components/InvoicePreview.tsx
@@ -19,28 +19,27 @@ const InvoicePreview = ({ item }: InvoiceProps) => {
     item.status[0].toUpperCase() + item.status.slice(1).toLowerCase();
 
   return (
-    <div className="grid auto-rows-auto grid-cols-2 gap-y-6 rounded-lg border border-white bg-white p-6 text-xs font-medium text-wild-blue-yonder hover:border hover:border-medium-slate-blue md:grid-cols-4 md:px-6 md:py-7">
-      <p className="font-bold">
+    <div className="grid auto-rows-auto grid-cols-2 rounded-lg border border-white bg-white p-6 text-xs font-medium text-wild-blue-yonder hover:border hover:border-medium-slate-blue md:grid-cols-4 md:px-6 md:py-7">
+      <p className="pb-6 font-bold">
         #<span className="text-cinder">{item.original_id}</span>
       </p>
-      <div className="justify-self-end">
+      <div className="justify-self-end pb-6">
         <p className="">{item.clientName}</p>
       </div>
 
-      <div className="flex flex-col justify-between space-y-2 md:flex-row">
-        <p className="">{formattedDate}</p>
-        <p className="text-base font-bold text-cinder">
-          £{new Intl.NumberFormat("en-US").format(item.total)}
-        </p>
-      </div>
+      <p className="row-start-2 row-end-3">{formattedDate}</p>
 
       <div
-        className={`flex h-10 w-[104px] items-center justify-center space-x-2 self-center justify-self-end rounded-md font-bold ${item.status === "paid" && "bg-aqua-green/5 text-aqua-green"} ${item.status === "pending" && "bg-dark-orange/5 text-dark-orange"} ${item.status === "draft" && "bg-bright-grey/5 text-bright-grey"}`}
+        className={`col-start-2 col-end-4 row-start-2 row-end-4 flex h-10 w-[104px] items-center justify-center space-x-2 self-center justify-self-end rounded-md font-bold ${item.status === "paid" && "bg-aqua-green/5 text-aqua-green"} ${item.status === "pending" && "bg-dark-orange/5 text-dark-orange"} ${item.status === "draft" && "bg-bright-grey/5 text-bright-grey"}`}
       >
         <span className="text-[32px]">·</span>
 
         <p className="">{statusString}</p>
       </div>
+
+      <p className="row-start-3 row-end-4 mt-2 text-base font-bold text-cinder">
+        £{new Intl.NumberFormat("en-US").format(item.total)}
+      </p>
     </div>
   );
 };
