8e162c214a28dc63557bc230e9b9d56777bd38bf
Xavi Guasch <xaviguasch@gmail.com>
2024-10-03 14:36:07 +0200

    refactored responsiveness for NewItem component, mobile view

--- app/_components/NewItem.tsx
+++ app/_components/NewItem.tsx
@@ -14,8 +14,15 @@ const NewItem = ({ item }) => {
   const total = qty * price;
 
   return (
-    <div className="flex flex-col items-stretch justify-start space-y-6">
-      <div className="flex flex-col items-stretch justify-start space-y-2.5">
+    <div className="grid grid-cols-[2fr_3fr_2fr_1fr] gap-x-4 gap-y-6">
+      <div className="hidden">
+        <span>Item Name</span>
+        <span>Qty.</span>
+        <span>Price</span>
+        <span>Total</span>
+      </div>
+
+      <div className="col-start-1 col-end-5 flex flex-col items-stretch justify-start space-y-2.5">
         <label
           htmlFor={`item-name-${item._id}`}
           className="text-xs text-wild-blue-yonder"
@@ -31,58 +38,57 @@ const NewItem = ({ item }) => {
         />
       </div>
 
-      <div className="flex flex-row items-stretch justify-between space-x-6">
-        <div className="flex w-1/6 flex-col items-stretch justify-start space-y-2.5">
-          <label
-            htmlFor={`item-qty-${item._id}`}
-            className="text-xs text-wild-blue-yonder"
-          >
-            Qty.
-          </label>
-          <input
-            type="number"
-            id={`item-qty-${item._id}`}
-            name={`item-qty-${item._id}`}
-            value={qty}
-            onChange={(e) => setQty(e.target.value)}
-            className="w-full rounded border border-link-water px-4 py-[14px] text-xs font-bold [appearance:textfield] focus:border-purple-mimosa md:text-base [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-          />
-        </div>
+      <div className="col-start-1 col-end-2 flex flex-col items-stretch justify-start space-y-2.5">
+        <label
+          htmlFor={`item-qty-${item._id}`}
+          className="text-xs text-wild-blue-yonder"
+        >
+          Qty.
+        </label>
+        <input
+          type="number"
+          id={`item-qty-${item._id}`}
+          name={`item-qty-${item._id}`}
+          value={qty}
+          onChange={(e) => setQty(e.target.value)}
+          className="w-full rounded border border-link-water px-4 py-[14px] text-xs font-bold [appearance:textfield] focus:border-purple-mimosa md:text-base [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+        />
+      </div>
 
-        <div className="flex w-2/6 flex-col items-stretch justify-start space-y-2.5">
-          <label
-            htmlFor={`item-price-${item._id}`}
-            className="text-xs text-wild-blue-yonder"
-          >
-            Price
-          </label>
-          <input
-            type="number"
-            id={`item-price-${item._id}`}
-            name={`item-price-${item._id}`}
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            className="w-full rounded border border-link-water px-5 py-[14px] text-xs font-bold [appearance:textfield] focus:border-purple-mimosa md:text-base [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
-          />
-        </div>
+      <div className="col-start-2 col-end-3 flex flex-col items-stretch justify-start space-y-2.5">
+        <label
+          htmlFor={`item-price-${item._id}`}
+          className="text-xs text-wild-blue-yonder"
+        >
+          Price
+        </label>
+        <input
+          type="number"
+          id={`item-price-${item._id}`}
+          name={`item-price-${item._id}`}
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          className="w-full rounded border border-link-water px-5 py-[14px] text-xs font-bold [appearance:textfield] focus:border-purple-mimosa md:text-base [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
+        />
+      </div>
 
-        <div className="relative flex w-3/6 flex-col items-stretch justify-start space-y-2.5">
-          <label
-            htmlFor={`item-total-${item._id}`}
-            className="text-xs text-wild-blue-yonder"
-          >
-            Total
-          </label>
-          <span
-            id={`item-total-${item._id}`}
-            className="w-full rounded px-5 py-[14px] text-xs font-bold focus:border-purple-mimosa md:text-base"
-          >
-            {total}
-          </span>
-          <div className="top- absolute right-2 top-[45%]">
-            <IconDeleteSVG />
-          </div>
-        </div>
+      <div className="relative col-start-3 col-end-4 flex w-3/6 flex-col items-stretch justify-start space-y-2.5">
+        <label
+          htmlFor={`item-total-${item._id}`}
+          className="text-xs text-wild-blue-yonder"
+        >
+          Total
+        </label>
+        <span
+          id={`item-total-${item._id}`}
+          className="w-full rounded py-[14px] text-xs font-bold focus:border-purple-mimosa md:text-base"
+        >
+          {total}
+        </span>
+      </div>
+
+      <div className="col-start-4 col-end-5 flex items-center justify-center pt-3">
+        <IconDeleteSVG />
       </div>
     </div>
   );
